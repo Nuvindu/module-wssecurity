a3721e393db4d1384f283bb374b6d3e6f2c095c0
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2023-09-06 12:11:17 +0530

    Improve APIs in the X509 token native class

--- native/src/main/java/org/wssecurity/X509SecToken.java
+++ native/src/main/java/org/wssecurity/X509SecToken.java
@@ -1,3 +1,19 @@
+// Copyright (c) 2023, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+//
+// WSO2 Inc. licenses this file to you under the Apache License,
+// Version 2.0 (the "License"); you may not use this file except
+// in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package org.wssecurity;
 
 import io.ballerina.runtime.api.utils.StringUtils;
@@ -7,20 +23,37 @@ import io.ballerina.runtime.api.values.BString;
 import org.apache.wss4j.common.crypto.Crypto;
 import org.apache.wss4j.common.crypto.CryptoFactory;
 import org.apache.wss4j.common.ext.WSSecurityException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
 import static org.wssecurity.Constants.NATIVE_UT;
 import static org.wssecurity.Constants.NATIVE_X509_TOKEN;
 import static org.wssecurity.Constants.X509;
+import static org.wssecurity.Utils.createError;
 
 public class X509SecToken  {
     private final Crypto crypto;
+    private final X509Certificate x509Certificate;
 
     public X509SecToken(BString filePath) {
+        FileInputStream fis;
         try {
+            fis = new FileInputStream(filePath.getValue());
             CertificateFactory certificateFactory = CertificateFactory.getInstance(X509);
+            this.x509Certificate = (X509Certificate) certificateFactory.generateCertificate(fis);
             this.crypto = CryptoFactory.getInstance(filePath.getValue());
-        } catch (WSSecurityException e) {
-            throw new RuntimeException(e);
+            fis.close();
+        } catch (CertificateException | WSSecurityException | IOException e) {
+            throw createError(e.getMessage());
         }
+    }
+
+    protected X509Certificate getX509Certificate() {
+        return x509Certificate;
     }
 
     public static void addX509Token(BObject x509Token, BObject userToken) {
@@ -35,15 +68,4 @@ public class X509SecToken  {
         return this.crypto;
     }
 
-//    protected X509Certificate getX509Certificate() {
-//        return x509Certificate;
-//    }
-//
-//    protected String getSignatureAlgoName() {
-//        return this.x509Certificate.getSigAlgName();
-//    }
-
-    public String getCustomTokenValueType() {
-        return WSConstants.X509TOKEN_NS + "#X509Token";
-    }
 }
